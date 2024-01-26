2602872a403967e34f4e32f89607f5450ea453b3
TimurMA <timur.minyazeff@gmail.com>
2024-01-26 23:30:09 +0500

    добавил запуск бэкенд-контейнеров в docker-compose.yml

--- comment-service/src/database/connection.ts
+++ comment-service/src/database/connection.ts
@@ -4,11 +4,11 @@ config({ path: '../.env' });
 
 const CONNECTION = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5434,
-  database: process.env.COMMENT_DATABASE ?? 'database',
-  username: process.env.COMMENT_USERNAME ?? 'username',
-  password: process.env.COMMENT_PASSWORD ?? 'password',
+  host: process.env.COMMENT_HOST || 'localhost',
+  port: +process.env.COMMENT_PORT || 5432,
+  database: process.env.COMMENT_DATABASE || 'database',
+  username: process.env.COMMENT_USERNAME || 'username',
+  password: process.env.COMMENT_PASSWORD || 'password',
 };
 
 export default CONNECTION;
--- main-app/src/database/connection.ts
+++ main-app/src/database/connection.ts
@@ -4,11 +4,11 @@ config({ path: '../.env' });
 
 const CONNECTION = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5435,
-  database: process.env.MAIN_DATABASE ?? 'database',
-  username: process.env.MAIN_USERNAME ?? 'username',
-  password: process.env.MAIN_PASSWORD ?? 'password',
+  host: process.env.MAIN_HOST || 'localhost',
+  port: +process.env.MAIN_PORT || 5435,
+  database: process.env.MAIN_DATABASE || 'database',
+  username: process.env.MAIN_USERNAME || 'username',
+  password: process.env.MAIN_PASSWORD || 'password',
 };
 
 export default CONNECTION;
